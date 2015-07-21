e3da3963da5bd3b7a533e871d78807c7d143a5b5
hatgirl <jls6190@rit.edu>
2015-07-21 13:53:48 -0400

    Added function for reducing a polynomial by the 2nth cyclotomic

--- Poly.hs
+++ Poly.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE RankNTypes #-}
 
 --import Math.Polynomial
+import Data.List
 import Data.Reflection
 import Data.Proxy
 import Text.Printf
@@ -43,13 +44,24 @@ instance (Integral a, Reifies c Integer) => Num (Poly c a) where
       abs = id
       signum (Poly [zero]) = Poly [0]
       signum _ = Poly [1]
-      fromInteger x = Poly ( modPolyByCyclo [fromInteger x] p)
+      fromInteger x = Poly (modPolyByCyclo [fromInteger x] p)
             where p = reflect (Proxy :: Proxy c)
 
 
 {-- Integer represents the index of the cyclotomic polynomial that we are
     using as a modulus. Should be a power of two or this is meaningless --}
 modPolyByCyclo :: (Integral a) => [a] -> Integer -> [a]
-modPolyByCyclo coeffs index
-  | (length coeffs) < index/2 = coeffs
-  | otherwise = undefined
+modPolyByCyclo coeffs index = secondFold firstFold
+-- First fold collapses the list onto twice the dimension of the result,
+-- giving it the same dimension as the Integer argument.
+-- Second fold collapses the list onto its proper dimension, subtracting the
+-- coefficients given in the second half of the list from those in the first.
+-- Since we're modding out by the nth cyclotomic polynomial, x^(n/2) + 1,
+-- x^(n/2) = -1, therefore
+-- the coefficients from n/2 to n-1 should be subtracted from the 0 to (n/2 -1) -- coefficients, the coefficients from n to n + (n/2 -1) should be added
+-- etc, etc
+  where firstFold = [sum [ coeffs!!x | x <- [0.. (genericLength coeffs) - 1], (toInteger x) `mod` index == i ] | i <- [0..(index - 1)] ] 
+        secondFold = (\x -> (genericTake (index `quot` (toInteger 2)) x) - ( genericDrop (index `quot` (toInteger 2)) x))
+
+
+

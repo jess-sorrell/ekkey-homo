2e12db63fba2d7cf3d30c03f53361e2eedad19a4
hatgirl <jls6190@rit.edu>
2015-08-04 17:43:43 -0400

    Resolved dimensionality issues for gadget inverse

--- Gadget.hs
+++ Gadget.hs
@@ -17,15 +17,42 @@ get_get_gadget modulus =
   let l = floor $ logBase 2 $ fromIntegral modulus
   in [ 2^i | i <- [0..l]]
 
+
 go_go_gadget_elem :: (Integral a) => a -> [a]
 go_go_gadget_elem 0 = [0]
 go_go_gadget_elem 1 = [1]
 go_go_gadget_elem x = (go_go_gadget_elem $ x `quot` 2)++[(x `mod` 2)]
 
+{--
+go_go_gadget_inverse' :: (Integral a) => [a] -> [[a]]
+go_go_gadget_inverse' as
+  | (filter ( <= 1) as) == as = [as]
+  | otherwise = (map (\x -> x `mod` 2) as):(go_go_gadget_inverse' $ map (\x -> x `quot` 2) as) --}
 
+go_go_gadget_inverse' :: (Integral a) => [a] -> [[a]]
+go_go_gadget_inverse' as = undefined
+
+
+
+go_go_gadget_inverse :: (Integral a) => a -> [a] -> [[a]]
+go_go_gadget_inverse modulus as =
+  let expand = go_go_gadget_inverse' $ map (\x -> x `mod` modulus ) as
+  in
+   let pad = (floor $ logBase 2 $ fromIntegral modulus) - (length expand)
+   in
+    expand++([ [0 | i <- [0..((length as) - 1)]] | j <- [0..pad]])
+
+
+
+
+{--
 go_go_gadget_vector :: (Integral a, Integral b) => b -> [a] -> [a]
 go_go_gadget_vector modulus [] = []
-go_go_gadget_vector modulus (x:xs) = (go_go_gadget_pad modulus x)++(go_go_gadget_vector modulus xs)
+go_go_gadget_vector modulus (x:xs) = (go_go_gadget_pad modulus x)++(go_go_gadget_vector modulus xs) --}
+
+go_go_gadget_invector :: (Integral a) => a -> [[a]] -> [[[a]]]
+go_go_gadget_invector modulus vec = map (go_go_gadget_inverse modulus) vec
+
 
 go_go_gadget_pad :: (Integral a, Integral b) => b -> a -> [a]
 go_go_gadget_pad modulus x =
@@ -35,6 +62,13 @@ go_go_gadget_pad modulus x =
    in
     ([0 | i <- [0..pad]]++xs)
 
+
+
+{--
+go_go_gadget_compose' :: (Integral a, Integral b) => a -> b -> [a] -> [[a]] -> [a]
+go_go_gadget_compose' modulus index as expanded --}
+
+{--
 go_go_gadget_compose :: (Integral a, Integral b) => a -> b -> [a] -> [a] -> [a]
 go_go_gadget_compose modulus index as expanded
   | length expanded == length as = convolve modulus index as expanded
@@ -45,6 +79,7 @@ go_go_gadget_compose modulus index as expanded
   | otherwise = error "What do you want me to do with this?"
 
 
-go_go_gadget_innerproduct :: (Integral a, Integral b) => a -> b -> [a] -> [a] -> [a]
-go_go_gadget_innerproduct modulus index as bs =
+go_go_gadget_product :: (Integral a, Integral b) => a -> b -> [a] -> [a] -> [a]
+go_go_gadget_product modulus index as bs =
   go_go_gadget_compose modulus index as (go_go_gadget_vector modulus bs)
+--}
